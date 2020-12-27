8e75bf10d48f449ec413a08891141f5c609d6e24
RaymondKerley <raymondkerley@outlook.com>
2020-12-27 18:44:49 +0000

    2 Player Game

--- TTT.py
+++ TTT.py
@@ -1,13 +1,7 @@
-currentBoard = {'7': '7' , '8': '8' , '9': '9' ,
-                '4': '4' , '5': '5' , '6': '6' ,
-                '1': '1' , '2': '2' , '3': '3' }
 
-symbols = ('X', 'O')
-playerTurn = (1,3,5,7,9) 
-computerTurn = (2,4,6,8)
-turn = 1
 
 from os import system, name  # define our clear function 
+
 def clear(): 
     # for windows 
     if name == 'nt': 
@@ -25,34 +19,75 @@ def printBoard(board):
     print('')
 
 
-for move in range(10):
-    clear()
-#player1
-    if turn in playerTurn:
-        printBoard(currentBoard)
-        selection = 'wrong'
-        
-        selection = input("It is your turn. Where would you like to place an X?")
-        while selection not in currentBoard or currentBoard[selection] in symbols:
-            clear()
-            printBoard(currentBoard)
-            selection = input("Incorrect selection. Where would you like to place an X?")  
-        currentBoard[selection] = 'X'   
-        turn +=1 
-    #printBoard(currentBoard)
 
-#player2
-    clear()
-    if turn in computerTurn:
-        printBoard(currentBoard)
-        selection = 'wrong'
+def playGame():
+    result = 'Not Over'
+    symbols = ('X', 'O')
+    turn = 1
+    playerTurn = (1,3,5,7,9) 
+    computerTurn = (2,4,6,8)
+
+    currentBoard = {'7': '7' , '8': '8' , '9': '9' ,
+                    '4': '4' , '5': '5' , '6': '6' ,
+                    '1': '1' , '2': '2' , '3': '3' }
+
+    while result == 'Not Over' and turn < 10:
         clear()
         printBoard(currentBoard)
-        selection = input("It is your turn. Where would you like to place an O?")
-        while selection not in currentBoard or currentBoard[selection] in symbols:
-            clear()
-            printBoard(currentBoard)
-            selection = input("Incorrect selection. Where would you like to place an O?")  
-        currentBoard[selection] = 'O'   
+        #player1
+        if turn in playerTurn:
+            selection = 'wrong'
+            selection = input("It is your turn. Where would you like to place an 'X'?")
+            while selection not in currentBoard or currentBoard[selection] in symbols:
+                clear()
+                printBoard(currentBoard)
+                selection = input("Incorrect selection. Where would you like to place an 'X'?")  
+            currentBoard[selection] = 'X'   
+        #player2
+        elif turn in computerTurn:
+            selection = 'wrong'
+            selection = input("It is your turn. Where would you like to place an 'O'?")
+            while selection not in currentBoard or currentBoard[selection] in symbols:
+                clear()
+                printBoard(currentBoard)
+                selection = input("Incorrect selection. Where would you like to place an 'O'?")  
+            currentBoard[selection] = 'O'   
+                
+        if turn >= 5:
+            if currentBoard['7'] == currentBoard['8'] == currentBoard['9']: 
+                result = "Game Over!"
+            elif currentBoard['4'] == currentBoard['5'] == currentBoard['6']:
+                result = "Game Over!"
+            elif currentBoard['1'] == currentBoard['2'] == currentBoard['3']:
+                result = "Game Over!"
+            elif currentBoard['7'] == currentBoard['4'] == currentBoard['1']:        
+                result = "Game Over!"
+            elif currentBoard['8'] == currentBoard['5'] == currentBoard['2']:
+                result = "Game Over!"
+            elif currentBoard['9'] == currentBoard['6'] == currentBoard['3']:
+                result = "Game Over!"
+            elif currentBoard['7'] == currentBoard['5'] == currentBoard['3']:
+                result = "Game Over!"
+            elif currentBoard['9'] == currentBoard['5'] == currentBoard['1']:
+                result = "Game Over!"
         turn +=1 
-   
+
+    clear()
+    printBoard(currentBoard)
+
+    if result == "Game Over!" and turn - 1 in playerTurn:
+        print(f"{result} Congratulations, You Won!") 
+    elif result == "Game Over!" and turn - 1 in computerTurn:
+        print(f"{result} Unlucky, You Lost!") 
+    else:
+        print("Game Over! Game Tied!") 
+
+
+newGame = "Y"
+
+while newGame == "Y":
+    playGame()
+    newGame = "choose"
+    while newGame not in ("Y", "N"):
+        newGame = input("Do you want to play again? (Y/N)").upper()
+    
