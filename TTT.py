326d918ee134aee45481c85b5961eacd26e2fea7
RaymondKerley <raymondkerley@outlook.com>
2020-12-27 15:52:47 +0000

    added clear function

--- TTT.py
+++ TTT.py
@@ -1,6 +1,20 @@
-theBoard = {'7': '7' , '8': '8' , '9': '9' ,
-            '4': '4' , '5': '5' , '6': '6' ,
-            '1': '1' , '2': '2' , '3': '3' }
+currentBoard = {'7': '7' , '8': '8' , '9': '9' ,
+                '4': '4' , '5': '5' , '6': '6' ,
+                '1': '1' , '2': '2' , '3': '3' }
+
+symbols = ('X', 'O')
+playerTurn = (1,3,5,7,9) 
+computerTurn = (2,4,6,8)
+turn = 1
+
+from os import system, name  # define our clear function 
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
             
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -8,22 +22,37 @@ def printBoard(board):
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('')
 
 
-theBoard['7'] = '7'
- 
-
-for turn in range(10):
-    printBoard(theBoard)
-    selection = 'wrong'
-    selection = input("It is your turn. Where would you like to place an X?")
-    while selection not in theBoard: #('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
-        selection = input("Incorrect selection. Where would you like to place an X?")
-        printBoard(theBoard)
+for move in range(10):
+    clear()
+#player1
+    if turn in playerTurn:
+        printBoard(currentBoard)
+        selection = 'wrong'
         
-'''
-         if theBoard[selection] != 'X' or 'Y':
-        # theBoard[selection] = 'X'
-        # else:
-        #     selection = ''
-'''
+        selection = input("It is your turn. Where would you like to place an X?")
+        while selection not in currentBoard or currentBoard[selection] in symbols:
+            clear()
+            printBoard(currentBoard)
+            selection = input("Incorrect selection. Where would you like to place an X?")  
+        currentBoard[selection] = 'X'   
+        turn +=1 
+    #printBoard(currentBoard)
+
+#player2
+    clear()
+    if turn in computerTurn:
+        printBoard(currentBoard)
+        selection = 'wrong'
+        clear()
+        printBoard(currentBoard)
+        selection = input("It is your turn. Where would you like to place an O?")
+        while selection not in currentBoard or currentBoard[selection] in symbols:
+            clear()
+            printBoard(currentBoard)
+            selection = input("Incorrect selection. Where would you like to place an O?")  
+        currentBoard[selection] = 'O'   
+        turn +=1 
+   
